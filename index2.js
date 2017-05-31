d62893a7ea3f6fdc1183291dc125618b82a7ba25
Vast Holdings <vastholdings@gmail.com>
2017-05-31 14:16:56 -0400

    Add maze

--- index2.js
+++ index2.js
@@ -1,38 +1,5 @@
 'use strict';
 
-var button = document.getElementById('button');
-var browserWidth = window.innerWidth || document.documentElement.clientWidth;
-var browserHeight = window.innerHeight || document.documentElement.clientHeight;
-var buttonWidth = button.offsetWidth;
-var buttonHeight = button.offsetHeight;
-var notset = 0;
-var lolz = ['Haha nope! Can\'t catch me', 'You are like tiny turtle, too slow', 'You will never find our treasures!!', 'HAHAHAHAHAAAAA'];
-
-function move() {
-    button.style.left = Math.floor(Math.random() * (browserWidth - buttonWidth)) + 'px';
-    button.style.top = Math.floor(Math.random() * (browserHeight - buttonHeight)) + 'px';
-    button.innerHTML = lolz[notset % lolz.length];
-    if (notset == 0) {
-        setTimeout(function () {
-            button.removeEventListener('mouseover', move);
-        }, 14000);
-    }
-    notset++;
-}
-
-if (typeof addEventListener !== 'undefined') {
-    button.addEventListener('mouseover', move, false);
-    button.addEventListener('click', function () {
-        alert('6420 E Forest Ave 48207\n\n\n\nbe there or be scared');
-    });
-} else if (typeof attachEvent !== 'undefined') {
-    button.attachEvent('onmouseover', move);
-    button.attachEvent('onclick', function () {
-        alert('6420 E Forest Ave 48207 be there or be scared');
-    });
-} else {
-    button.onmousover = move;
-}
 
 window.addEventListener("load", function () {
     var elements = document.getElementsByClassName("rainbowText");
@@ -52,3 +19,10 @@ function generateRainbowText(element) {
     }
 }
 
+
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
