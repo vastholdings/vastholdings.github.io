09bb6a5d0878e78afc75fc099e288df22f0faae0
Colin <colin.diesh@gmail.com>
2017-09-01 12:42:44 -0400

    Rename old games

--- js/game.js
+++ js/game.js
@@ -4,5 +4,16 @@
 
 
 var gameboard = document.getElementById('gameboard');
-var ctx = gameboard.getContext2d()
-console.log(ctx);
+var ctx = gameboard.getContext('2d')
+
+
+
+ctx.strokeStyle='red';
+ctx.fillStyle='black';
+ctx.lineTo(0,0);
+ctx.lineTo(0,100);
+ctx.lineTo(100,100);
+ctx.lineTo(100,0);
+ctx.stroke();
+ctx.fill();
+
