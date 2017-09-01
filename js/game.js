ad4a674cce9921edea2be1152fb0cce908a7416c
Colin <colin.diesh@gmail.com>
2017-09-01 13:36:08 -0400

    Add game

--- js/game.js
+++ js/game.js
@@ -1,9 +1,15 @@
 'use strict';
 
 
+var clouds = [];
+var cloudsy = [];
+for(var i = 0; i < 10000; i++) {
+    clouds[i] = 150*i + 100*(Math.random()-0.5);
+    cloudsy[i] = Math.random()*20;
+}
 
+var clown = 1000;
 
-var obstacles = [1000,2000];
 
 document.addEventListener("DOMContentLoaded", function() {
     var gameboard = document.getElementById('gameboard');
@@ -32,6 +38,45 @@ document.addEventListener("DOMContentLoaded", function() {
         for(var i = 0; i < 20; i++) {
             ctx.fillRect(i*10, -2*Math.floor(10*Math.sin(i*Math.PI/6+t/1000)), 10, -10);
         }
+
+        ctx.setTransform(1,0,0,1,0,0);
+        ctx.translate(-t/100,0);
+        ctx.fillStyle='white';
+        for(var i = 0; i < clouds.length; i++) {
+            ctx.fillRect(clouds[i], cloudsy[i], 20, 20);
+        }
+
+
+
+
+        ctx.fillStyle = 'red';
+        ctx.translate(clown,h*3/4-50);
+        ctx.fillRect(0, 0, 10, 10);
+        ctx.fillRect(10, 0, 10, 10);
+        ctx.fillRect(20, 0, 10, 10);
+        ctx.fillRect(30, 0, 10, 10);
+        ctx.fillRect(30, 0, 10, 10);
+        ctx.fillRect(40, 0, 10, 10);
+        ctx.fillRect(30, -10, 10, 10);
+        ctx.fillRect(20, -10, 10, 10);
+        ctx.fillRect(10, -10, 10, 10);
+        ctx.fillRect(20, -20, 10, 10);
+        ctx.fillStyle='yellow';
+        ctx.translate(0,10);
+        ctx.fillRect(0,0,10,10);
+        ctx.fillRect(10,0,10,10);
+        ctx.fillRect(20,0,10,10);
+        ctx.fillRect(30,0,10,10);
+        ctx.fillRect(40,0,10,10);
+        ctx.fillRect(20,10,10,10);
+        ctx.fillRect(30,10,10,10);
+        ctx.fillRect(10,10,10,10);
+        ctx.fillRect(0,10,10,10);
+        ctx.fillRect(40,10,10,10);
+        ctx.fillRect(20,20,10,10);
+        ctx.fillRect(30,20,10,10);
+        ctx.fillRect(10,20,10,10);
+
     }
     setInterval(function() {
         t+=70;
