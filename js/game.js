db9b9c3180d1dfbd3b3567fb8b1b38d0edf45128
Vast Holdings <vastholdings@gmail.com>
2017-09-18 23:27:21 -0400

    Make drawClown modular

--- js/game.js
+++ js/game.js
@@ -4,7 +4,6 @@ var flag_win = false;
 var flag_lose = false;
 var clouds = [];
 var cloudsy = [];
-
 var fireballs = [];
 
 for(var i = 0; i < 10000; i++) {
@@ -22,6 +21,7 @@ var clownVelocity = 10;
 var hitRadius = 0;
 var w;
 var h;
+var timestep = 70;
 
 
 function drawBackground(ctx) {
@@ -46,13 +46,95 @@ function drawWorm(ctx, t) {
 function drawClouds(ctx, t) {
     ctx.fillStyle='white';
     for(var i = 0; i < clouds.length; i++) {
-        var x = clouds[i]-t/100;
-        var x2 = x+20;
+        clouds[i] -= timestep/10;
+        var x = clouds[i];
+        var x2 = clouds[i]+20;
         ctx.fillRect(x-x%10, cloudsy[i], 80, 20);
         ctx.fillRect(x2-x2%10, cloudsy[i]-20, 40, 20);
     }
 }
 
+function drawFireballs(ctx) {
+    ctx.translate(0,h*3/4-10);
+    for(var i = 0; i < fireballs.length; i++) {
+        var f = fireballs[i];
+        ctx.fillStyle = 'red';
+        ctx.fillRect(f.x - f.x % 10, f.y, 10, 20);
+        ctx.fillStyle = 'orange';
+        ctx.fillRect(f.x - f.x % 10 + 10, f.y, 10, 20);
+        ctx.fillStyle = 'yellow';
+        ctx.fillRect(f.x - f.x % 10 + 20, f.y, 10, 20);
+        f.x += timestep/10;
+    }
+}
+
+function drawClown(ctx) {
+    ctx.translate(0,h*3/4-10);
+    //hat
+    ctx.fillStyle='red';
+    ctx.translate(clown-clown%10, h*3/4-50);
+    ctx.fillRect(0, 0, 10, 10);
+    ctx.fillRect(10, 0, 10, 10);
+    ctx.fillRect(20, 0, 10, 10);
+    ctx.fillRect(30, 0, 10, 10);
+    ctx.fillRect(30, 0, 10, 10);
+    ctx.fillRect(40, 0, 10, 10);
+    ctx.fillRect(30, -10, 10, 10);
+    ctx.fillRect(20, -10, 10, 10);
+    ctx.fillRect(10, -10, 10, 10);
+    ctx.fillRect(20, -20, 10, 10);
+    //face
+    ctx.fillStyle='yellow';
+    ctx.translate(0,10);
+    ctx.fillRect(0,0,10,10);
+    ctx.fillRect(10,0,10,10);
+    ctx.fillRect(20,0,10,10);
+    ctx.fillRect(30,0,10,10);
+    ctx.fillRect(40,0,10,10);
+    ctx.fillRect(20,10,10,10);
+    ctx.fillRect(30,10,10,10);
+    ctx.fillRect(10,10,10,10);
+    ctx.fillRect(0,10,10,10);
+    ctx.fillRect(40,10,10,10);
+    ctx.fillRect(20,20,10,10);
+    ctx.fillRect(30,20,10,10);
+    ctx.fillRect(10,20,10,10);
+    ctx.fillRect(10,30,10,10);
+    ctx.fillRect(20,30,10,10);
+    ctx.fillRect(30,30,10,10);
+    //eyes
+    ctx.strokeStyle='black';
+    ctx.beginPath();
+    ctx.moveTo(10,5);
+    ctx.lineTo(15,15);
+    ctx.moveTo(10,15);
+    ctx.lineTo(15,5);
+    ctx.translate(20,0);
+    ctx.moveTo(10,5);
+    ctx.lineTo(15,15);
+    ctx.moveTo(10,15);
+    ctx.lineTo(15,5);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fillStyle='red';
+    ctx.fillRect(0,20,10,10);
+    clown-=timestep/10;
+
+
+}
+
+
+function checkCollisions() {
+    for(var i = 0; i < fireballs.length; i++) {
+        var f = fireballs[i];
+        if(Math.abs(f.x - clown) < 20 && !flag_win) {
+            flag_win = true;
+            hitLocx = f.x;
+            hitLocy = f.y;
+            clownVelocity = 0;
+        }
+    }
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     var gameboard = document.getElementById('gameboard');
@@ -89,98 +171,32 @@ document.addEventListener("DOMContentLoaded", function() {
             ctx.fillText('clown: haha I killed u u dum worm!!', 0, 200);
             ctx.fillText('       you gon be crushd now', 0, 220);
         }
-
-
-        ctx.translate(0,h*3/4-10);
-        for(var i = 0; i < fireballs.length; i++) {
-            var f = fireballs[i];
-            ctx.fillStyle = 'red';
-            ctx.fillRect(f.x - f.x % 10, f.y, 10, 20);
-            ctx.fillStyle = 'orange';
-            ctx.fillRect(f.x - f.x % 10 + 10, f.y, 10, 20);
-            ctx.fillStyle = 'yellow';
-            ctx.fillRect(f.x - f.x % 10 + 20, f.y, 10, 20);
-            f.x+=10;
-
-            if(Math.abs(f.x - clown) < 20 && !flag_win) {
-                flag_win = true;
-                hitLocx = f.x;
-                hitLocy = f.y;
-                clownVelocity = 0;
-               
-            }
-            if(flag_win) {
-                ctx.fillStyle = 'brown';
-                for(var i = 0; i <= 8; i++) {
-                    ctx.fillRect(hitLocx - hitLocx % 10 + hitRadius*Math.cos(Math.PI/8*i), hitLocy - hitRadius*Math.sin(Math.PI/8*i), 10, 10);
-                }
-                ctx.fillStyle = 'red';
-                for(var i = 0; i <= 8; i++) {
-                    ctx.fillRect(hitLocx - hitLocx % 10 + (hitRadius-10)*Math.cos(Math.PI/8*i), hitLocy - (hitRadius-10)*Math.sin(Math.PI/8*i), 10, 10);
-                }
-                hitRadius +=10;
-                ctx.fillStyle = 'red';
-                ctx.fillText('worm: HAHA U JUST GOT FIREBALLZD TELL ME', 0, -200);
-                ctx.fillText('      WHERE THE SHOW IS', 0, -180);
-                ctx.fillStyle = 'darkgreen';
-                ctx.fillText('clown: owowow it is at 6420 e forest!', 0, -100);
-            }
-        }
+        ctx.save();
+        drawFireballs(ctx);
         ctx.restore();
-        //ctx.translate(-10*Math.floor(t/1000),0);
-        
 
+        ctx.save();
+        drawClown(ctx);
+        ctx.restore();
 
+        checkCollisions();
 
-        //hat
-        ctx.fillStyle='red';
-        ctx.translate(clown-clown%10, h*3/4-50);
-        ctx.fillRect(0, 0, 10, 10);
-        ctx.fillRect(10, 0, 10, 10);
-        ctx.fillRect(20, 0, 10, 10);
-        ctx.fillRect(30, 0, 10, 10);
-        ctx.fillRect(30, 0, 10, 10);
-        ctx.fillRect(40, 0, 10, 10);
-        ctx.fillRect(30, -10, 10, 10);
-        ctx.fillRect(20, -10, 10, 10);
-        ctx.fillRect(10, -10, 10, 10);
-        ctx.fillRect(20, -20, 10, 10);
-        //face
-        ctx.fillStyle='yellow';
-        ctx.translate(0,10);
-        ctx.fillRect(0,0,10,10);
-        ctx.fillRect(10,0,10,10);
-        ctx.fillRect(20,0,10,10);
-        ctx.fillRect(30,0,10,10);
-        ctx.fillRect(40,0,10,10);
-        ctx.fillRect(20,10,10,10);
-        ctx.fillRect(30,10,10,10);
-        ctx.fillRect(10,10,10,10);
-        ctx.fillRect(0,10,10,10);
-        ctx.fillRect(40,10,10,10);
-        ctx.fillRect(20,20,10,10);
-        ctx.fillRect(30,20,10,10);
-        ctx.fillRect(10,20,10,10);
-        ctx.fillRect(10,30,10,10);
-        ctx.fillRect(20,30,10,10);
-        ctx.fillRect(30,30,10,10);
-        //eyes
-        ctx.strokeStyle='black';
-        ctx.beginPath();
-        ctx.moveTo(10,5);
-        ctx.lineTo(15,15);
-        ctx.moveTo(10,15);
-        ctx.lineTo(15,5);
-        ctx.translate(20,0);
-        ctx.moveTo(10,5);
-        ctx.lineTo(15,15);
-        ctx.moveTo(10,15);
-        ctx.lineTo(15,5);
-        ctx.closePath();
-        ctx.stroke();
-        ctx.fillStyle='red';
-        ctx.fillRect(0,20,10,10);
-        clown-=clownVelocity;
+        if(flag_win) {
+            ctx.fillStyle = 'brown';
+            for(var i = 0; i <= 8; i++) {
+                ctx.fillRect(hitLocx - hitLocx % 10 + hitRadius*Math.cos(Math.PI/8*i), hitLocy - hitRadius*Math.sin(Math.PI/8*i), 10, 10);
+            }
+            ctx.fillStyle = 'red';
+            for(var i = 0; i <= 8; i++) {
+                ctx.fillRect(hitLocx - hitLocx % 10 + (hitRadius-10)*Math.cos(Math.PI/8*i), hitLocy - (hitRadius-10)*Math.sin(Math.PI/8*i), 10, 10);
+            }
+            hitRadius +=10;
+            ctx.fillStyle = 'red';
+            ctx.fillText('worm: HAHA U JUST GOT FIREBALLZD TELL ME', 0, -200);
+            ctx.fillText('      WHERE THE SHOW IS', 0, -180);
+            ctx.fillStyle = 'darkgreen';
+            ctx.fillText('clown: owowow it is at 6420 e forest!', 0, -100);
+        }
 
         
 
@@ -200,5 +216,5 @@ document.addEventListener("DOMContentLoaded", function() {
         t+=100;
         s++;
         drawGame();
-    }, 70);
+    }, timestep);
 });
